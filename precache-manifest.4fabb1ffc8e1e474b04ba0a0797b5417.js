85ac1429d9264b535ac80f46358f387a3d2e34f3
kazirafi71 <kazirafi66@gmail.com>
2020-10-06 17:56:12 +0600

    Updates

---
+++ precache-manifest.4fabb1ffc8e1e474b04ba0a0797b5417.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13036d96c28cf649ee4622b456dd0aa",
+    "url": "/reactweb2/index.html"
+  },
+  {
+    "revision": "ecfa747e5d7aa26276a9",
+    "url": "/reactweb2/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "27113ee4229fdaece198",
+    "url": "/reactweb2/static/css/main.df9949d7.chunk.css"
+  },
+  {
+    "revision": "ecfa747e5d7aa26276a9",
+    "url": "/reactweb2/static/js/2.969cbd0e.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactweb2/static/js/2.969cbd0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27113ee4229fdaece198",
+    "url": "/reactweb2/static/js/main.012dcc52.chunk.js"
+  },
+  {
+    "revision": "00c55847b076f137a2c1",
+    "url": "/reactweb2/static/js/runtime-main.823654d8.js"
+  },
+  {
+    "revision": "66ebec837af80149a7f23887aa54c9de",
+    "url": "/reactweb2/static/media/IPhone.66ebec83.jpg"
+  },
+  {
+    "revision": "a82524ecbb011f619bde4205654fefe3",
+    "url": "/reactweb2/static/media/Iphn11.a82524ec.jpg"
+  },
+  {
+    "revision": "00fb687dabee03d0042da0721d4ba443",
+    "url": "/reactweb2/static/media/Mack.00fb687d.jpg"
+  },
+  {
+    "revision": "c162369e794949b97cb19601ea24faeb",
+    "url": "/reactweb2/static/media/Talk.c162369e.jpg"
+  },
+  {
+    "revision": "c0d0fb789edee3ccbd4bb97ebb1d62a0",
+    "url": "/reactweb2/static/media/pexels-format-1029757.c0d0fb78.jpg"
+  },
+  {
+    "revision": "b99cde3dbc1260dc8c08554ac268aa11",
+    "url": "/reactweb2/static/media/pexels-pixabay-265667.b99cde3d.jpg"
+  },
+  {
+    "revision": "e79e23bc96bbd293e16c8553710d196a",
+    "url": "/reactweb2/static/media/pexels-tranmautritam-251225.e79e23bc.jpg"
+  }
+]);
